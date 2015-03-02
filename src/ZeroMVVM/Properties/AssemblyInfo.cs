b116a74e21a8127b0e1cccf7472774a18a98d078
Cameron MacFarland <distantcam@gmail.com>
2015-03-02 14:31:41 +0800

    Added versioning and packaging

--- src/ZeroMVVM/Properties/AssemblyInfo.cs
+++ src/ZeroMVVM/Properties/AssemblyInfo.cs
@@ -9,18 +9,5 @@ using System.Windows.Markup;
 [assembly: AssemblyCompany("Cameron MacFarland")]
 [assembly: AssemblyProduct("ZeroMVVM")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 // XAML Namespaces
 [assembly: XmlnsDefinition("http://zeromvvm.github.com/", "ZeroMVVM.XAML")]
